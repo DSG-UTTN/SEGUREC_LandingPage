e5e531f4dc8c8bb54fbf68a164655e206f3402f0
DSG99 <daniel.santos@uttn.mx>
2025-09-30 12:20:25 -0500

    tailwind.css reference updated

--- templates/layout.php
+++ templates/layout.php
@@ -35,7 +35,7 @@ $seo = $seo ?? [];
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     
     <!-- TailwindCSS - Local Build -->
-    <link href="<?= publicUrl('css/tailwind.css') ?>" rel="stylesheet">
+    <link href="/public/css/tailwind.css" rel="stylesheet">
     
     <!-- Schema.org JSON-LD -->
 
